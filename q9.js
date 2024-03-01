a5e9020c19a0c57eb35b11b7a6c008ab546b71b2
SowmiyaGuvi <sowmimiya43@gmail.com>
2024-03-02 04:11:35 +0530

    q9 file commit

---
+++ q9.js
@@ -0,0 +1,28 @@
+//Write a code to get the input in the given format and print the output in the given format.
+
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+
+const str = userInput[0];
+  
+  let i = 0;
+  while (i < str.length){
+      console.log(str[i]);
+      i++;
+  }
+  //end-here
+});
