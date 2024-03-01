a762db7474584399149309000835f64dcc33592a
SowmiyaGuvi <sowmimiya43@gmail.com>
2024-03-02 04:14:59 +0530

    q10 file commit

---
+++ q10.js
@@ -0,0 +1,23 @@
+//Write a code to get the input in the given format and print the output in the given format.
+
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+
+  console.log(userInput[0].split('').join(','));
+
+  //end-here
+});
